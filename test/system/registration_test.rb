cf95bd854653d57a0968b20a525a49460bc8bba3
Marat Mamiashev <mmamiashev@metaquotes.net>
2018-05-24 18:02:13 +0300

    preparare

--- test/system/registration_test.rb
+++ test/system/registration_test.rb
@@ -49,4 +49,78 @@ class UsersTest < ApplicationSystemTestCase
     assert_text I18n.t('devise.sessions.signed_out')
    
   end
+  
+  
+  #TODO
+  context "with invalid details" do
+
+    # before do
+    #   visit new_user_registration_path
+    # end
+    #
+    # scenario "blank fields" do
+    #
+    #   expect_fields_to_be_blank
+    #
+    #   click_button "Sign up"
+    #
+    #   expect(page).to have_error_messages "Email can't be blank",
+    #     "Password can't be blank"
+    # end
+    #
+    # scenario "incorrect password confirmation" do
+    #
+    #   fill_in "Email", with: "tester@example.tld"
+    #   fill_in "Password", with: "test-password"
+    #   fill_in "Password confirmation", with: "not-test-password"
+    #   click_button "Sign up"
+    #
+    #   expect(page).to have_error_message "Password confirmation doesn't match Password"
+    # end
+    #
+    # scenario "already registered email" do
+    #
+    #   create(:user, email: "dave@example.tld")
+    #
+    #   fill_in "Email", with: "dave@example.tld"
+    #   fill_in "Password", with: "test-password"
+    #   fill_in "Password confirmation", with: "test-password"
+    #   click_button "Sign up"
+    #
+    #   expect(page).to have_error_message "Email has already been taken"
+    # end
+    #
+    # scenario "invalid email" do
+    #
+    #   fill_in "Email", with: "invalid-email-for-testing"
+    #   fill_in "Password", with: "test-password"
+    #   fill_in "Password confirmation", with: "test-password"
+    #   click_button "Sign up"
+    #
+    #   expect(page).to have_error_message "Email is invalid"
+    # end
+    #
+    # scenario "too short password" do
+    #
+    #   min_password_length = 8
+    #   too_short_password = "p" * (min_password_length - 1)
+    #   fill_in "Email", with: "someone@example.tld"
+    #   fill_in "Password", with: too_short_password
+    #   fill_in "Password confirmation", with: too_short_password
+    #   click_button "Sign up"
+    #
+    #   expect(page).to have_error_message "Password is too short (minimum is 8 characters)"
+    # end
+
+  end
+
+  private
+
+  def expect_fields_to_be_blank
+    # expect(page).to have_field("Email", with: "", type: "email")
+    # # These password fields don't have value attributes in the generated HTML,
+    # # so with: syntax doesn't work.
+    # expect(find_field("Password", type: "password").value).to be_nil
+    # expect(find_field("Password confirmation", type: "password").value).to be_nil
+  end
 end
